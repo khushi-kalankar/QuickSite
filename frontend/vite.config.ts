bda831d16007fc6e746d7f19c540bfc4118e096e
Khushi <khushikalankar@gmail.com>
2025-06-22 13:08:54 +0530

    frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin"
+    }
+  }
 })
